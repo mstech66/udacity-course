0f982af029dafcf837164e833b0c9332d21908f6
mstech66 <manthanmevada66@gmail.com>
2019-04-18 16:55:58 +0530

    Lesson 6 Code

--- course/06_input/task_06_input/lib/converter_route.dart
+++ course/06_input/task_06_input/lib/converter_route.dart
@@ -40,7 +40,41 @@ class ConverterRoute extends StatefulWidget {
 class _ConverterRouteState extends State<ConverterRoute> {
   // TODO: Set some variables, such as for keeping track of the user's input
   // value and units
+  Unit fromValue;
+  Unit toValue;
+  double input;
+  String converted = '';
+  bool _showError = false;
+  List<DropdownMenuItem> _unitMenuItem;
 
+  @override
+  void initState() {
+    super.initState();
+    _createDropDownItems();
+    _setDefaults();
+  }
+
+  void _setDefaults() {
+    setState(() {
+      fromValue = widget.units[0];
+      toValue = widget.units[1];
+    });
+  }
+
+  void _createDropDownItems() {
+    var newItems = <DropdownMenuItem>[];
+    for (var unit in widget.units) {
+      newItems.add(DropdownMenuItem(
+        value: unit.name,
+        child: Container(
+          child: Text(unit.name, softWrap: true),
+        ),
+      ));
+    }
+    setState(() {
+      _unitMenuItem = newItems;
+    });
+  }
   // TODO: Determine whether you need to override anything, such as initState()
 
   // TODO: Add other helper functions. We've given you one, _format()
@@ -61,41 +95,134 @@ class _ConverterRouteState extends State<ConverterRoute> {
     return outputNum;
   }
 
+  Widget _createDropdown (String currentVal, ValueChanged<dynamic> onChanged){
+    return Container(
+      margin: EdgeInsets.only(top: 16.0),
+      decoration: BoxDecoration(
+        color: Colors.grey[50],
+        border: Border.all(
+          color: Colors.grey[400],
+          width: 1.0
+        )
+      ),
+      padding: EdgeInsets.symmetric(vertical: 8.0),
+      child: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.grey[50]
+        ),
+        child: DropdownButtonHideUnderline(
+          child: ButtonTheme(
+            alignedDropdown: true,
+            child: DropdownButton(
+              value: currentVal,
+              items: _unitMenuItem,
+              onChanged: onChanged,
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  Unit _getUnit(String unitName) {
+    return widget.units.firstWhere(
+      (Unit unit) {
+        return unit.name == unitName;
+      },
+      orElse: null,
+    );
+  }
+  void _updateConversion() {
+    setState(() {
+      converted =
+          _format(input * (toValue.conversion / fromValue.conversion));
+    });
+  }
+  void _updateFromConversion(dynamic unitName) {
+    setState(() {
+      fromValue = _getUnit(unitName);
+    });
+    if (input != null) {
+      _updateConversion();
+    }
+  }
+  void _updateToConversion(dynamic unitName) {
+    setState(() {
+      toValue = _getUnit(unitName);
+    });
+    if (input != null) {
+      _updateConversion();
+    }
+  }
   @override
   Widget build(BuildContext context) {
     // TODO: Create the 'input' group of widgets. This is a Column that
     // includes the input value, and 'from' unit [Dropdown].
+    final input = Padding(
+      padding: EdgeInsets.all(16.0),
+      child: new Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TextField(
+               decoration: InputDecoration(
+                 labelStyle: Theme.of(context).textTheme.display1,
+                 labelText: "Input",
+                 border: OutlineInputBorder(
+                   borderRadius: BorderRadius.circular(0.0)
+                 )
+               ),
+               keyboardType: TextInputType.number,
+            ),
+            _createDropdown(fromValue.name, _updateFromConversion)
+          ],
+        ),
+      ),
+    );
 
     // TODO: Create a compare arrows icon.
 
+    final arrows = RotatedBox(
+      child: Icon(Icons.compare_arrows, size: 40),
+      quarterTurns: 1,
+    );
+
     // TODO: Create the 'output' group of widgets. This is a Column that
     // includes the output value, and 'to' unit [Dropdown].
-
-    // TODO: Return the input, arrows, and output widgets, wrapped in a Column.
-
-    // TODO: Delete the below placeholder code.
-    final unitWidgets = widget.units.map((Unit unit) {
-      return Container(
-        color: widget.color,
-        margin: EdgeInsets.all(8.0),
-        padding: EdgeInsets.all(16.0),
+    final output = Padding(
+      padding: EdgeInsets.all(16.0),
+      child: new Container(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Text(
-              unit.name,
-              style: Theme.of(context).textTheme.headline,
+            TextField(
+               decoration: InputDecoration(
+                 labelStyle: Theme.of(context).textTheme.display1,
+                 labelText: "Output",
+                 border: OutlineInputBorder(
+                   borderRadius: BorderRadius.circular(0.0)
+                 )
+               ),
+               keyboardType: TextInputType.number,
             ),
-            Text(
-              'Conversion: ${unit.conversion}',
-              style: Theme.of(context).textTheme.subhead,
-            ),
+            _createDropdown(toValue.name, _updateToConversion)
           ],
         ),
-      );
-    }).toList();
-
-    return ListView(
-      children: unitWidgets,
+      ),
     );
+
+    // TODO: Return the input, arrows, and output widgets, wrapped in a Column.
+    final converter =  Column(
+      children: <Widget>[
+        input,
+        arrows,
+        output
+      ],
+    );
+    return Padding(
+      child: converter,
+      padding: EdgeInsets.all(16.0),
+    );
+    // TODO: Delete the below placeholder code.
   }
 }
